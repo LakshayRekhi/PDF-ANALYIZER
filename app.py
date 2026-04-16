0c83f5494ee994b11febe8cfef267aaae8861940
Lakshay Rekhi <127512416+LakshayRekhi@users.noreply.github.com>
2026-04-16 23:52:52 +0530

    Update print statement from 'Hello' to 'Goodbye'

--- app.py
+++ app.py
@@ -15,7 +15,6 @@ from financial_ocr import FinancialPDFExtractor, ChartReadyNormalizer
 # ─────────────────────────────────────────────
 # BOOTSTRAP
 # ─────────────────────────────────────────────
-GROQ_API_KEY="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"
 load_dotenv()
 st.set_page_config(page_title="Financial Analyst", layout="wide", page_icon="📊")
 
@@ -166,7 +165,7 @@ st.markdown("""
 </style>
 """, unsafe_allow_html=True)
 
-api_key = GROQ_API_KEY
+api_key = os.getenv("GROQ_API_KEY")
 if not api_key:
     st.error("🚨 GROQ_API_KEY is missing from your .env file.")
     st.stop()
